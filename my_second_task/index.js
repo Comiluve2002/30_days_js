cd18c937ee992808c18f91159c98d13f0e1bb341
Comiluve2002 <68999564+Comiluve2002@users.noreply.github.com>
2021-08-20 14:41:45 +0100

    updated

---
+++ my_second_task/index.js
@@ -0,0 +1,12 @@
+let name= 'Comfort';
+let stateOfOrigin='Kogi';
+let age='25';
+let gender='female';
+let nationality='Nigerian';
+let religion='christianity';
+let genotype='AA';
+let maritalStatus='single';
+
+console.log( name, stateOfOrigin, age, gender, nationality, religion, genotype, maritalStatus);
+
+
